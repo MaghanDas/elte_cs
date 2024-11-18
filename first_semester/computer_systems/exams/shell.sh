bd934d460013c9fe2385284ea7e5ec533507b73e
Maghan Das <dasmaghan360@gmail.com>
2024-11-18 14:33:20 +0100

    Update shell.sh

---
+++ first_semester/computer_systems/exams/shell.sh
@@ -0,0 +1,4 @@
+Comsys Courses has 6 total Exams
+2 lab Exams ( Bash Shell & Powershell Exam ( worth 20 points )
+2 Theory Exam (Bash Shell & Powershell Exam -> MCQS( worth 10 points )
+2 HomeWork -> 1 For bash shell and 1 for Poweshell (worth 1 point each)
